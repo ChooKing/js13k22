bb076986baa0f8fd008da5ffa9ec26adb8b1d10c
ChooKing <38577527+ChooKing@users.noreply.github.com>
2022-09-10 17:09:05 -0400

    Zombie skin, hair, shirt, pants, and shoe colors randomized

--- src/util.ts
+++ src/util.ts
@@ -2,3 +2,6 @@ import {RGB} from "./types";
 export const rgb2str=(rgb: RGB)=>{
     return `rgb(${rgb.r},${rgb.g},${rgb.b})`;
 }
+export const dark=(c:RGB,f:number)=>{
+    return {r:Math.floor(c.r*f),g:Math.floor(c.g*f),b:Math.floor(c.b*f)}
+}
