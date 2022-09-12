b80f6ce8e84798b55a2685b4ded371d190f81143
ChooKing <38577527+ChooKing@users.noreply.github.com>
2022-09-12 17:30:23 -0400

    Procedurally add platforms

--- src/game.ts
+++ src/game.ts
@@ -8,25 +8,36 @@ export const Game = {
     ctx: undefined as CanvasRenderingContext2D|undefined,
     cw: 1600, //canvas width
     ch: 900, //canvas height
-    ww:16000, //world width
-    wh:9000, //world height
+    ww:14000, //world width
+    wh:6000, //world height
     cx:0, //cameraX
     cy:0, //cameraY
     th: 240, //tile height
     tw: 350, //tile width
     tyo: 65, //tile Y offset
+    tr: 25, //tile rows
+    tc: 40, //tile columns
     ns: 1200, //speed
     ls: false, //like side movement
+    pe:200, //platform edge - avoid placing zombies beyond this position
+    zsp:-100,//zombie speed
     init:()=>{
-        Game.ps=[
-            new Plat(0, 8, 1),
-            new Plat(1,7,2),
-            new Plat(3, 5, 2),
-            new Plat(3, 2, 2),
-            new Plat(5, 4, 1),
-            new Plat(6,3,1)
-        ]
+        for(let r=3;r<Game.tr;r++){
+            for(let c=0;c<Game.tc;c++){
+                if(r%3==0){
+                    if(c%3==0){
+                        Game.ps.push(new Plat(c,r,1))
+                    }
+                }
+                else{
+                    if((c-1)%3==0){
+                        Game.ps.push(new Plat(c,r,2))
+                    }
+                }
 
+            }
+
+        }
     },
     ps: [] as Array<Plat>,
     zs: [] as Array<Zombie>,
